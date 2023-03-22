7eeb8483d86d700b6a0d4d8c964fd06d4621d3d9
Matheus Biaggio <matheusbiaggio@gmail.com>
2023-03-21 21:51:58 -0300

    Criação de validação para o field Name

--- src/middlewares/validadeNameField.js
+++ src/middlewares/validadeNameField.js
@@ -2,16 +2,16 @@ const validadeNameField = (req, res, next) => {
   const { name } = req.body;
 
   if (!name) {
-    return res.status(404).json({ type: 'INVALID_VALUE', message: 'Nome precisa ser preenchido' });
+    return res.status(400).json({ type: 'INVALID_VALUE', message: '"name" is required' });
   }
 
   if (typeof name !== 'string') {
     return res.status(404).json({ type: 'INVALID_VALUE', message: 'Nome precisa ser uma string' });
   }
 
-  if (name.length < 2) {
-    return res.status(404).json(
-      { type: 'INVALID_VALUE', message: 'Nome precisa ser maior que 2 letras' },
+  if (name.length < 5) {
+    return res.status(422).json(
+      { type: 'INVALID_VALUE', message: '"name" length must be at least 5 characters long' },
     );
   }
 
